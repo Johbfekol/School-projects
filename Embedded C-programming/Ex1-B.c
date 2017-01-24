5588d40555af56da16af87ac3704740f1d1e484d
Heeboja <samun91@hotmail.com>
2017-01-24 18:02:41 +0200

    Create Ex1-B.c

---
+++ Embedded C-programming/Ex1-B.c
@@ -0,0 +1,73 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 24.1.2017 16:08:31
+ * Author : u97479
+ */ 
+
+#define F_CPU 8000000UL // CPU speed @ 8 MHz
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+
+static inline uint8_t debounce(volatile uint8_t *, const uint8_t);
+static inline uint8_t blink(void);
+
+
+int main(void)
+{
+    DDRB = 0xFF;  // PORTB as input
+    PORTB = 0xFF; // PORTB default values off
+    DDRD = 0x00;  // PORTD as inputs
+	uint8_t number = 0;
+	uint8_t counter = 0;
+	while (PIND&0x80) {   // Loops until SW7 is pressed
+		counter++;
+		} 
+
+	srand(counter);   // generates a random seed based on the counter valule
+	number = rand() % 3; // a random number from 0 to 2
+	number++;
+
+	while (1) 
+    {
+		if(debounce(&PIND, PD0) && number == 1 ) {
+			blink();
+			PORTB =~ number;
+		}
+		if(debounce(&PIND, PD1)&& number == 2 ) {
+			blink();
+			PORTB =~ number;
+		}
+		if(debounce(&PIND, PD2)&& number == 3 ) {
+			blink();
+			PORTB =~ number;
+		}
+    }
+	return 0;
+}
+
+static inline uint8_t blink(void) {
+	for (int i = 0; i < 5; i++) {
+		PORTB = 0x00;
+		_delay_ms(500);
+		PORTB =~ PORTB;
+		_delay_ms(500);
+	}
+	return 0;
+}
+
+static inline uint8_t debounce(volatile uint8_t *pinReg, const uint8_t pin) {
+	if(!(*pinReg & (1 << pin))) {
+		//pin grounded wait 100ms
+		_delay_ms(100);
+		
+		if(*pinReg & (1 << pin)) {
+			// give the user time to release the button
+			_delay_ms(100);
+			return 1;
+		}
+	}
+	return 0;
+}
