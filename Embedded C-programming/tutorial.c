290248f29171b04b89af8786ca5f687bdb9d4f56
Johbfekol <johbfeko@hotmail.com>
2017-01-26 13:19:58 +0200

    Update tutorial.c

--- Embedded C-programming/tutorial.c
+++ Embedded C-programming/tutorial.c
@@ -4,14 +4,14 @@
 int main(void) {
 
 	DDRB = 0xFF; // port B is output
-	PORTB = 0xFE; // PORTB: 1111 1110 only the first led is on 
+	PORTB = 0xFE; // PORTB: 1111 1110 only the first led is 'on' 
 	_delay_ms(200); // delay
 	
 	while(1) { // eternal loop. This loop turns all leds on one by one.
-               // Shifts all bits by one first, so when its 0x00 (all leds on,) it turns all of and starts bit
+               // Shifts all bits by one first, so when its 0x00 (all leds 'on',) it turns all of and starts bit
 			   // shifting again. This happens with 600 ms intervals.
-		if(PORTB == 0) PORTB = 0xFF; // if all leds on, turn off all, PORTB = 1111 1111
-		PORTB <<= 1; // leds go on one by one
+		if(PORTB == 0) PORTB = 0xFF; // if all leds 'on', turn off all, PORTB = 1111 1111
+		PORTB <<= 1; // leds go 'on' one by one
 		_delay_ms(600); // delay
 		
 	}
